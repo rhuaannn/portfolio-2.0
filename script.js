35dc10dd9a213d75f2ad928d3389a74dc597050f
rhuaannn <rhuanmarques1223@gmail.com>
2022-12-23 11:40:28 -0300

    add to menu hamburgue

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function clickMenu() {
     if (itens.style.display == "block"){
         itens.style.display = "none";
     }else {
-        itens.style.display = "block"
+       itens.style.display = "none"
     }
 }
